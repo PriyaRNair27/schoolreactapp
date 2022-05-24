975c4f38279ad06acbcce66ed6c0294bc76b01a1
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-24 12:20:22 +0530

    search student

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addstudent from './Components/Addstudent';
+import Searchstudent from './Components/Searchstudent';
 
 function App() {
   return (
     <div>
       <Addstudent/>
+      <Searchstudent/>
 
     </div>
   );
